5dce4b610dc21ac7d53fcdc67206456a14b38348
ohey <huynhgiabao853@gmail.com>
2025-08-07 11:12:42 +0700

    hi1

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://scorelens-backend.onrender.com',
+  baseURL: 'https://scorelens-backend.onrender.com/api',
 });
 
 instance.interceptors.request.use(
